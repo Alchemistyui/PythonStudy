9264bf94d61494c191a8961f5dd74a6c97ccb76f
Alchemist <1319525622@qq.com>
2018-01-19 22:29:30 +0800

    first test

---
+++ test.py
@@ -0,0 +1,15 @@
+# name = input('enter your name')
+
+# print('hello', name)
+
+# print('\\\t\\')
+
+# print(r'\\\t\\')
+
+print(' line1 \n line2 \n line3')
+print('''line1
+    aaa
+line2
+line3''')
+
+
