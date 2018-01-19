eb8dbb956f847b274a02bc11ce0d826105b6431c
Alchemist <1319525622@qq.com>
2018-01-19 22:32:14 +0800

    edit

--- test.py
+++ test.py
@@ -11,4 +11,15 @@ print('''line1
 line2
 line3''')
 
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+for i in n,f,s1,s2,s3,s4:
+    print(i)
+
 
