e466e37c0f9788c84fe6d5ad48c0d6fd36528b70
Alchemist <1319525622@qq.com>
2018-01-30 20:33:18 +0800

    learn many..

--- test.py
+++ test.py
@@ -126,46 +126,122 @@
 
 
 #8.函数
-def my_abs(x):
-    if not isinstance(x, (int,float)): 
-        raise TypeError('bad operand type')
-    if x >= 0:
-        return x
-    else:
-        return -x
+# def my_abs(x):
+#     if not isinstance(x, (int,float)): 
+#         raise TypeError('bad operand type')
+#     if x >= 0:
+#         return x
+#     else:
+#         return -x
 
-print(my_abs(-233))
+# print(my_abs(-233))
 
-def null_fun():
-    pass
+# def null_fun():
+#     pass
 
-def nextTwo(x=1):
-    if not isinstance(x, int):
-        raise TabError('bad operand type')
-    return x+1, x+2
-#默认参数必须指向不变对象！
-x,y = nextTwo()
-print(x,y)
+# def nextTwo(x=1):
+#     if not isinstance(x, int):
+#         raise TabError('bad operand type')
+#     return x+1, x+2
+# #默认参数必须指向不变对象！
+# x,y = nextTwo()
+# print(x,y)
 
-def summit(*number):
-    sum = 0
-    for i in number:
-        sum = sum + i 
-    return sum
+# def summit(*number):
+#     sum = 0
+#     for i in number:
+#         sum = sum + i 
+#     return sum
 
-nums = [1,2,3,4,5,6,7,8,9,10]
-print(summit(*nums))
+# nums = [1,2,3,4,5,6,7,8,9,10]
+# print(summit(*nums))
 
-def person(name,**other):
-    print(name, 'other:', other)
+# def person(name,**other):
+#     print(name, 'other:', other)
 
-person('yun')
-person('dabiao',city='Beijing')
-d = {'age': 18, 'city': "chendu"}
-person('alchemist',**d)
+# person('yun')
+# person('dabiao',city='Beijing')
+# d = {'age': 18, 'city': "chendu"}
+# person('alchemist',**d)
 
-#命名关键字参数可以有缺省值
-def person2(name, age, *, city, job):
-    print(name, age, city, job)
-person2('Jack', 24, city='Beijing', job='Engineer')
-#参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+# #命名关键字参数可以有缺省值
+# def person2(name, age, *, city, job):
+#     print(name, age, city, job)
+# person2('Jack', 24, city='Beijing', job='Engineer')
+# #参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+
+
+#9. 切片:list,tuple,字符串
+# l = list(range(100))
+# print(l[0:10])
+
+
+
+#10.迭代:list,tuple,字符串,dict
+# d = {'a': 1, 'b': 2, 'c': 3}
+# for key in d:
+#     print(key)
+
+# for value in d.values():
+#     print(value)
+# for k,v in d.items():
+#     print(k,':',v)
+
+# for i, value in enumerate(['A', 'B', 'C']):
+#     print(i, value)
+
+#11.列表生成式
+# print([x * x for x in range(1,11) if x % 2 == 0])
+# print([x + y for x in 'abc' for y in 'xyz'])
+# import os
+# print([d for d in os.listdir('.')])
+
+#12.生成器
+# g = (x * x for x in range(10))
+# print(g)
+# for i in g:
+#     print(i)
+
+# def fib(max):
+#     n,a,b = 0,0,1
+#     while(n<max):
+#         yield(b)
+#         a,b = b,a+b
+#         n = n + 1
+#     return 'done'
+
+# f = fib(6)
+# for i in f:
+#     print(i)
+
+
+# def triangles():
+#     Y=[1]    
+#     while True:
+#         yield Y
+#         Y=[1]+[Y[i]+Y[i+1] for i in range(len(Y)-1)]+[1]
+# n = 0
+# results = []
+# for t in triangles():
+#     print(t)
+#     results.append(t)
+#     n = n + 1
+#     if n == 10:
+#         break
+
+#13.迭代器
+# it = iter([1, 2, 3, 4, 5])
+
+#14.高阶函数
+
+def is_palindrome(n):
+    s=str(n)
+    t=s[::-1]
+    return s==t
+      
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
